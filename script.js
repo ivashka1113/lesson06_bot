1748e8a892dd929f8305089d908c2c2db86f52de
Ivan Scherbakov <ivashka1113@mail.ru>
2022-10-06 22:27:04 +0700

    Выполнил домашнее задание 6

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 "use strict";
+
+const isNumber = function (number) {
+    return !isNaN(parseFloat(number)) && isFinite(number) && !/\s/g.test(number);
+}
+
+const bot = function () {
+
+    let num = Math.round(Math.random() * 100);
+    console.log(num);
+
+    return function gues() {
+        let ans = prompt("Угадай число от 1 до 100", 54);
+        console.log(ans);
+
+        if (ans === null) {
+            return alert("Игра окончена");
+        } else if (!isNumber(ans)) {
+            alert("Введи число!");
+            gues();
+        }
+
+        switch (true) {
+
+            case (ans < num):
+                alert("Загаданное число больше, попробуй ещё раз");
+                gues();
+                break;
+            case (ans > num):
+                alert("Загаданное число меньше, попробуй ещё раз");
+                gues();
+                break;
+            default:
+                alert("Поздравляю, вы угадали");
+                break;
+        }
+    }
+}
+
+const startGame = bot();
+startGame();
