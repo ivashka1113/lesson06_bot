8b39472e602cad7be250c9682ad4df004bc6f209
Ivan Scherbakov <ivashka1113@mail.ru>
2022-10-06 22:47:48 +0700

    Расширил функционал бота

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+let oneMore;
+
 const isNumber = function (number) {
     return !isNaN(parseFloat(number)) && isFinite(number) && !/\s/g.test(number);
 }
@@ -8,13 +10,20 @@ const bot = function () {
 
     let num = Math.round(Math.random() * 100);
     console.log(num);
+    let k = 10;
+
 
     return function gues() {
+        if (k == 0) {
+            return oneMore = confirm("Игра окончена, хотели бы сыграть еще?");
+        }
+
         let ans = prompt("Угадай число от 1 до 100", 54);
         console.log(ans);
 
         if (ans === null) {
-            return alert("Игра окончена");
+            return oneMore = confirm("Игра окончена, хотели бы сыграть еще?");
+
         } else if (!isNumber(ans)) {
             alert("Введи число!");
             gues();
@@ -23,19 +32,25 @@ const bot = function () {
         switch (true) {
 
             case (ans < num):
-                alert("Загаданное число больше, попробуй ещё раз");
+                alert("Загаданное число больше, осталось " + k + " попыток");
+                k--;
                 gues();
                 break;
             case (ans > num):
-                alert("Загаданное число меньше, попробуй ещё раз");
+                alert("Загаданное число меньше, осталось " + k + " попыток");
+                k--;
                 gues();
                 break;
             default:
-                alert("Поздравляю, вы угадали");
+                oneMore = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
                 break;
         }
     }
 }
 
-const startGame = bot();
-startGame();
+do {
+    const startGame = bot();
+    startGame();
+} while (oneMore === true);
+
+alert("Спасибо, что поиграли в мою игру");
