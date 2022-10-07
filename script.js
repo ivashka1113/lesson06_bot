dbfda0f9cd89076aa8ba4fb358950a68cf4735ee
Ivan Scherbakov <ivashka1113@mail.ru>
2022-10-07 18:47:16 +0700

    Создал вторую версию задания с вызовом внутренней функции

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 "use strict";
 
-let oneMore;
-
-const newGame = function () {
-    const startGame = bot();
-    startGame();
-    if (oneMore === true) newGame();
-}
 
 const isNumber = function (number) {
     return !isNaN(parseFloat(number)) && isFinite(number) && !/\s/g.test(number);
@@ -17,16 +10,17 @@ const bot = function () {
     let num = Math.round(Math.random() * 100);
     console.log(num);
     let k = 10;
+    let oneMore;
 
 
-    return function gues() {
+    const gues = function () {
+
         if (k == 0) {
             return oneMore = confirm("Игра окончена, хотели бы сыграть еще?");
         }
 
-        let ans = prompt("Угадай число от 1 до 100", 54);
+        let ans = prompt("Угадай число от 0 до 100", 54);
         console.log(ans);
-
 
         switch (true) {
             case (ans === null):
@@ -35,20 +29,30 @@ const bot = function () {
             case (!isNumber(ans)):
                 alert("Введи число!");
                 break;
+
             case (ans < num):
                 alert("Загаданное число больше, осталось " + k + " попыток");
                 k--;
                 break;
+
             case (ans > num):
                 alert("Загаданное число меньше, осталось " + k + " попыток");
                 k--;
                 break;
+
             default:
                 return oneMore = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
         }
         gues();
     }
+
+    gues();
+
+    if (oneMore === true) {
+        bot();
+    } else if (oneMore === false) {
+        alert("Спасибо, что поиграли в мою игру");
+    }
 }
 
-newGame();
-alert("Спасибо, что поиграли в мою игру");
+bot();
