5a19e76d6217693631395eca4663ab161b144ecb
Ivan Scherbakov <ivashka1113@mail.ru>
2022-10-07 08:03:40 +0700

    Внес правки, цикл остался

--- script.js
+++ script.js
@@ -21,30 +21,26 @@ const bot = function () {
         let ans = prompt("Угадай число от 1 до 100", 54);
         console.log(ans);
 
-        if (ans === null) {
-            return oneMore = confirm("Игра окончена, хотели бы сыграть еще?");
-
-        } else if (!isNumber(ans)) {
-            alert("Введи число!");
-            gues();
-        }
 
         switch (true) {
+            case (ans === null):
+                return oneMore = confirm("Игра окончена, хотели бы сыграть еще?");
 
+            case (!isNumber(ans)):
+                alert("Введи число!");
+                break;
             case (ans < num):
                 alert("Загаданное число больше, осталось " + k + " попыток");
                 k--;
-                gues();
                 break;
             case (ans > num):
                 alert("Загаданное число меньше, осталось " + k + " попыток");
                 k--;
-                gues();
                 break;
             default:
-                oneMore = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
-                break;
+                return oneMore = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
         }
+        gues();
     }
 }
 
